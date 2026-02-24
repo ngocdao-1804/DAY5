c802d6e138bb05e76353abbe71514dc8aee931c3
ngocdao-1804 <accloneofdao@gmail.com>
2026-02-24 20:56:23 +0700

    update2

--- index.js
+++ index.js
@@ -3,14 +3,25 @@
 // time === 5; in đã qua 1/2 thời gian
 // còn lại: còn time giây
 // xong loop Hết giờ
-let time = 10;
+
+// let time = 10;
+// while (time > 0) {
+//     console.log(`còn ${time} giây`)
+//     time--;
+//     if (time === 5) {
+//         console.log(`đã qua nửa thời gian`)
+//     }
+// } 
+// console.log(`Hết giờ `)
+
+let time = 20;
+let halftime= time/2;
 while (time > 0) {
-    time--;
-    console.log(`còn ${time} giây`)
-    if (time === 5) {
-        console.log(`đã qua nửa thời gian`)
-       
-    }
-    
-} 
-console.log(`Hết giờ `)
+  if (time === halftime) {
+    console.log("Đã qua nửa thời gian");
+  } else {
+    console.log(`Còn lại ${time} giây`);
+  }
+  time--;
+}
+console.log("Đã hết giờ ");
