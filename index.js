1bb352f8d5e3da00c612606575e0a2200b03e1b4
ngocdao-1804 <accloneofdao@gmail.com>
2026-02-26 12:40:56 +0700

    update3

--- index.js
+++ index.js
@@ -3,25 +3,58 @@
 // Total
 // For, 1 gia ran cho sp, cộng dồn vào total
 // in tong tien phai tra
+// let total = 0;
+
+
+//Fixed bai 2:
+const NUMBER_OF_ORDERS = 5;
 let total = 0;
 
+for (let i = 0; i < NUMBER_OF_ORDERS; i++) {
+  let price = Math.ceil(Math.random() * 5000000);
 
-for(let numOfOrder = 5; numOfOrder>=1; numOfOrder--){
-    let price = Math.round((Math.random()*5000000))
-if (price < 100000){
-    price = 100000
+  if (price < 100000) {
+    price = 100000;
+  }
+
+  total += price;
+
+  console.log(
+    `Tổng tiền sau đơn hàng thứ ${i + 1} là: ${total.toLocaleString()}VNĐ`,
+  );
 }
-    console.log(`Tổng tiền phải trả là ${total+price} `)
-}
 
-// random (0,1) (Math.round(Math.random()))
-// 0: không giảm 1: giảm giá random tử 10 -50%
+console.log(`Tổng tiền phải trả là: ${total.toLocaleString()}VNĐ`);
 
-
+//Bai làm:
+//  let total = 0;
 
 
+// for(let numOfOrder = 5; numOfOrder>=1; numOfOrder--){
+//     let price = Math.round((Math.random()*5000000))
+// if (price < 100000){
+//     price = 100000
+// }
+//     console.log(`Tổng tiền phải trả là ${total+price} `)
+// }
 
 
+
+
+// /Fixed bai 1
+// // let time = 21;
+// const halfTime = Math.floor(time / 2);
+
+// while (time > 0) {
+//   time--;
+//   if (time === halfTime) {
+//     console.log("Đã qua nửa thời gian");
+//   } else {
+//     console.log(`Còn lại ${time} giây`);
+//   }
+// }
+
+// console.log("Hết giờ");
 
 // BÀI 1: khai báo biến time 10
 // lopp while dk time > 0
